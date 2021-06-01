c81a01f935707d8e7216b44f69b96aba21dabf86
DemendiAkos <akosdemendi@gmail.com>
2021-06-01 19:58:37 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+  $('.navbar-nav li').click(function (x) {
+    $('.navbar-nav li').removeClass('active');
+    $(event.target).parent().addClass('active');
+  });
+});
+
+function showCircuit() {
+    $('#circuit').change(function (o) {
+      switch (o.target.value) {
+        case 'ITA':
+          $('#circuitimage').attr('src', 'img/monza.jpg');
+          $('#f1sound')[0].play();
+          break;
+        case 'HUN':
+          $('#circuitimage').attr('src', 'img/hungaroring.jpg');
+          break;
+        case 'BEL':
+          $('#circuitimage').attr('src', 'img/spa.jpg');
+          break;
+        case 'MON':
+          $('#circuitimage').attr('src', 'img/monaco.jpg');
+          break;
+
+        default:
+          break;
+      }
+    });
+  }
+
+//---------------------------------------------------------
+
+function calculate() {
+  let map = document.querySelector('#circuit').value;
+  let sped = document.querySelector('#laptime').value/3600;
+      
+  let value = 0;
+  
+  
+
+  let final = map/sped;
+
+  let slot = document.querySelector('#averagespeed');
+  slot.value = final + " km/h";
+
+  console.log(map + " " + sped + " " + final)
+  
+}
+
+
